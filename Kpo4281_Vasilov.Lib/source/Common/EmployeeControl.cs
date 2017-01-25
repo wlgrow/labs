564a8047b8eb55c2d5478b986f60806943ad7956
wlgrow <maxim24_89@mail.ru>
2017-01-25 21:55:33 +0300

    3rd
    3

---
+++ Kpo4281_Vasilov.Lib/source/Common/EmployeeControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kpo4281.Vasilov.Lib
+{
+    public static class EmployeeControl
+    {
+        public static bool InfoControl(Employee employee)
+        {
+            if (string.IsNullOrWhiteSpace(employee.surname) || string.IsNullOrWhiteSpace(employee.initials))
+            {
+                return false;
+            }
+            else if(employee.birth<1900 || employee.birth>2010)
+            {
+                return false;
+            }
+            else if (employee.salary<0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Kpo4281_Vasilov.Lib/source/loader/EmployeeListSplitFileLoader.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Kpo4281.Vasilov.Lib
+{
+    public class EmployeeListSplitFileLoader :IEmployeeListLoader
+    {
+        
+        public EmployeeListSplitFileLoader(string dataFileName)
+        {
+            _dataFileName = dataFileName;
+        }
+
+        private readonly string _dataFileName = null;
+        private List<Employee> _employeeList = new List<Employee>();
+        private LoadStatus _status = LoadStatus.None;
+
+        public List<Employee> employeeList()
+        {
+            return _employeeList;
+        }
+        public LoadStatus status
+        {
+            get { return _status; }
+        }
+
+        private void SplitLine(string source)
+        {
+            try
+            {
+                string[] arr = source.Split('|');
+                if (arr.Length < 4)
+                {
+                    _status = LoadStatus.GeneralError;
+                    throw new System.IndexOutOfRangeException("В файле в строке не хватает данных");
+                }
+                Employee employee = new Employee()
+                {
+                    surname = arr[0],
+                    initials = arr[1],
+                    birth = Int32.Parse(arr[2]),
+                    salary = float.Parse(arr[3], System.Globalization.CultureInfo.InvariantCulture)
+                };
+                _employeeList.Add(employee);
+            }
+            catch (Exception ex)
+            {
+                LogUtility.ErrorLog(DateTime.Now + " Ошибка: " + ex.Message + "\n");
+                _status = LoadStatus.GeneralError;
+            }
+        }
+
+        public void Execute()
+        {
+            if (string.IsNullOrWhiteSpace(_dataFileName))
+            {
+                _status=LoadStatus.FileNameIsEmpty;
+                throw new Exception("У файла не может быть пустого имени");
+            }
+            else if (!File.Exists(_dataFileName))
+            {
+                _status = LoadStatus.FileNotExists;
+                _employeeList = null;
+                throw new FileNotFoundException(_dataFileName);
+            }
+
+            StreamReader sr = null;
+            using (sr = new StreamReader(_dataFileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    //Прочитать очередную строку
+                    string str = sr.ReadLine();
+                    SplitLine(str);
+                }
+            }
+            if (_status == LoadStatus.None)
+            {
+                _status = LoadStatus.Success;
+            }
+        }
+
+    }
+}
